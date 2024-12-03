c4b656de523ef3cf0526bd9b6522d31f82507884
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-03 22:51:47 +0100

    add turtle player

--- core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
+++ core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
@@ -10,10 +10,12 @@ class MovingPlayerKotlin : Game() {
 
     private lateinit var batch: SpriteBatch
     private lateinit var image: Texture
+    private lateinit var turtle: Texture
 
     override fun create() {
         batch = SpriteBatch()
         image = Texture("Game BG.png")
+        turtle = Texture("turtle.png")
     }
 
     override fun render() {
@@ -24,6 +26,7 @@ class MovingPlayerKotlin : Game() {
 
         batch.begin()
         batch.draw(image, 0f, 0f, screenWidth.toFloat(), screenHeight.toFloat())
+        batch.draw(turtle, (screenWidth/2).toFloat(), 0f, 200f, 200f)
         batch.end()
     }
 
