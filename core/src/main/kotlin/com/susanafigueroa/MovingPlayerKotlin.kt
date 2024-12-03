75d9ea1edd4dc6a989b8efe29b6e57ce50cdcc6d
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-03 21:21:02 +0100

    MovingPlayerKotlin extends Game

--- core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
+++ core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
@@ -1,6 +1,29 @@
 package com.susanafigueroa
 
-import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.ScreenUtils
 
-/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
-class MovingPlayerKotlin : ApplicationAdapter()
+class MovingPlayerKotlin : Game() {
+
+    private lateinit var batch: SpriteBatch
+    private lateinit var image: Texture
+
+    override fun create() {
+        batch = SpriteBatch()
+        image = Texture("logo.png")
+    }
+
+    override fun render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f)
+        batch.begin()
+        batch.draw(image, 140f, 210f)
+        batch.end()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        image.dispose()
+    }
+}
