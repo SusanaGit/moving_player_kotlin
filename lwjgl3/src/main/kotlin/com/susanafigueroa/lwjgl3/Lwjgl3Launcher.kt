23a2595febee0ed4b151b1c14f66a5afa38a1c3c
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-03 19:59:16 +0100

    create project

---
+++ lwjgl3/src/main/kotlin/com/susanafigueroa/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,19 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package com.susanafigueroa.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.susanafigueroa.MovingPlayerKotlin
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    // This handles macOS support and helps on Windows.
+    if (StartupHelper.startNewJvmIfRequired())
+      return
+    Lwjgl3Application(MovingPlayerKotlin(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("moving_player_kotlin")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
