e57079c37a0b705b620074f806ff5761d347a3a1
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-04 10:53:41 +0100

    using OrthographicCamera and StretchViewport to use the WIDTH and the HEIGHT of the GameInfo to declare thing positions and sizes

--- core/src/main/java/helpers/GameInfo.kt
+++ core/src/main/java/helpers/GameInfo.kt
@@ -5,8 +5,8 @@ class GameInfo {
 
     // to define static members of the class
     companion object {
-        const val WIDTH = 640
-        const val HEIGHT = 480
+        const val WIDTH = 480
+        const val HEIGHT = 800
     }
 
 }
--- core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
+++ core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
@@ -2,9 +2,12 @@ package com.susanafigueroa
 
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.utils.viewport.StretchViewport
+import helpers.GameInfo
 
 class MovingPlayerKotlin : Game() {
 
@@ -12,7 +15,21 @@ class MovingPlayerKotlin : Game() {
     private lateinit var image: Texture
     private lateinit var turtle: Texture
 
+    // I want to use the WIDTH and the HEIGHT of the GameInfo to declare thing positions and sizes
+    // OrthographicCamera -> defines 2D perspective of the game
+    private lateinit var camera: OrthographicCamera
+    // StretchViewport -> to maintain a fixed aspect ratio
+    private lateinit var viewport: StretchViewport
+
     override fun create() {
+        // initialize camera and viewport
+        camera = OrthographicCamera()
+        viewport = StretchViewport(GameInfo.WIDTH.toFloat(), GameInfo.HEIGHT.toFloat(), camera)
+
+        // set the position of the camera in the middle of the screen
+        camera.position.set(GameInfo.WIDTH/2f, GameInfo.HEIGHT/2f, 0f)
+        camera.update()
+
         batch = SpriteBatch()
         image = Texture("Game BG.png")
         turtle = Texture("turtle.png")
@@ -21,17 +38,22 @@ class MovingPlayerKotlin : Game() {
     override fun render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f)
 
-        val screenWidth = Gdx.graphics.width
-        val screenHeight = Gdx.graphics.height
+        camera.update()
+        batch.setProjectionMatrix(camera.combined);
 
         batch.begin()
-        batch.draw(image, 0f, 0f, screenWidth.toFloat(), screenHeight.toFloat())
-        batch.draw(turtle, (screenWidth/2).toFloat(), 0f, 200f, 200f)
+        batch.draw(image, 0f, 0f, GameInfo.WIDTH.toFloat(), GameInfo.HEIGHT.toFloat())
+        batch.draw(turtle, (GameInfo.WIDTH/2).toFloat(), 0f, 200f, 200f)
         batch.end()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        viewport.update(width, height)
     }
 
     override fun dispose() {
         batch.dispose()
         image.dispose()
+        turtle.dispose()
     }
 }
