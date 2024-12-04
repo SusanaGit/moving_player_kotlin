6bb548dd36403cf2b0be3c89c9992ff49445a434
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-04 23:13:57 +0100

    add OrthographicCamera and StretchViewport in MainMenu

--- core/src/main/java/scenes/MainMenu.kt
+++ core/src/main/java/scenes/MainMenu.kt
@@ -1,7 +1,10 @@
 package scenes
 
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.utils.viewport.StretchViewport
 import com.susanafigueroa.MovingPlayerKotlin
 import helpers.GameInfo
 
@@ -9,22 +12,40 @@ class MainMenu (private var movingPlayerKotlin: MovingPlayerKotlin) : Screen {
 
     private var imageBackground: Texture
 
+    // I want to use the WIDTH and the HEIGHT of the GameInfo to declare thing positions and sizes
+    // OrthographicCamera -> defines 2D perspective of the game
+    private lateinit var camera: OrthographicCamera
+
+    // StretchViewport -> to maintain a fixed aspect ratio
+    private lateinit var viewport: StretchViewport
+
     init {
         imageBackground = Texture("Game BG.png")
     }
 
     override fun show() {
+        // initialize camera and viewport
+        camera = OrthographicCamera()
+        viewport = StretchViewport(GameInfo.WIDTH.toFloat(), GameInfo.HEIGHT.toFloat(), camera)
 
+        // set the position of the camera in the middle of the screen
+        camera.position.set(GameInfo.WIDTH/2f, GameInfo.HEIGHT/2f, 0f)
+        camera.update()
     }
 
     override fun render(delta: Float) {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f)
+
+        camera.update()
+        movingPlayerKotlin.getBatch.setProjectionMatrix(camera.combined)
+
         movingPlayerKotlin.getBatch.begin()
         movingPlayerKotlin.getBatch.draw(imageBackground, 0f, 0f, GameInfo.WIDTH.toFloat(), GameInfo.HEIGHT.toFloat())
         movingPlayerKotlin.getBatch.end()
     }
 
     override fun resize(width: Int, height: Int) {
-
+        viewport.update(width, height)
     }
 
     override fun pause() {
@@ -40,7 +61,8 @@ class MainMenu (private var movingPlayerKotlin: MovingPlayerKotlin) : Screen {
     }
 
     override fun dispose() {
-
+        movingPlayerKotlin.getBatch.dispose()
+        imageBackground.dispose()
     }
 
 }
--- core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
+++ core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
@@ -29,20 +29,11 @@ class MovingPlayerKotlin : Game() {
     /*
     private lateinit var turtle: Sprite
 
-    // I want to use the WIDTH and the HEIGHT of the GameInfo to declare thing positions and sizes
-    // OrthographicCamera -> defines 2D perspective of the game
-    private lateinit var camera: OrthographicCamera
-    // StretchViewport -> to maintain a fixed aspect ratio
-    private lateinit var viewport: StretchViewport
+
+
 
     override fun create() {
-        // initialize camera and viewport
-        camera = OrthographicCamera()
-        viewport = StretchViewport(GameInfo.WIDTH.toFloat(), GameInfo.HEIGHT.toFloat(), camera)
 
-        // set the position of the camera in the middle of the screen
-        camera.position.set(GameInfo.WIDTH/2f, GameInfo.HEIGHT/2f, 0f)
-        camera.update()
 
 
 
@@ -52,20 +43,15 @@ class MovingPlayerKotlin : Game() {
     }
 
     override fun render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f)
-
-        camera.update()
-        batch.setProjectionMatrix(camera.combined);
-        batch.draw(turtle, turtle.x, turtle.y, 200f, 200f)
+batch.draw(turtle, turtle.x, turtle.y, 200f, 200f)
     }
 
     override fun resize(width: Int, height: Int) {
-        viewport.update(width, height)
+
     }
 
     override fun dispose() {
-        batch.dispose()
-        image.dispose()
+
         turtle.getTexture().dispose()
     }*/
 }
