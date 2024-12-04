46a15e31b8ae875a175ca5b1a8575648eb82c25a
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-04 23:31:01 +0100

    add turtle

--- core/src/main/java/scenes/MainMenu.kt
+++ core/src/main/java/scenes/MainMenu.kt
@@ -3,14 +3,13 @@ package scenes
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.StretchViewport
 import com.susanafigueroa.MovingPlayerKotlin
 import helpers.GameInfo
 
 class MainMenu (private var movingPlayerKotlin: MovingPlayerKotlin) : Screen {
-
-    private var imageBackground: Texture
 
     // I want to use the WIDTH and the HEIGHT of the GameInfo to declare thing positions and sizes
     // OrthographicCamera -> defines 2D perspective of the game
@@ -19,9 +18,8 @@ class MainMenu (private var movingPlayerKotlin: MovingPlayerKotlin) : Screen {
     // StretchViewport -> to maintain a fixed aspect ratio
     private lateinit var viewport: StretchViewport
 
-    init {
-        imageBackground = Texture("Game BG.png")
-    }
+    private lateinit var imageBackground: Texture
+    private lateinit var turtle: Sprite
 
     override fun show() {
         // initialize camera and viewport
@@ -31,6 +29,10 @@ class MainMenu (private var movingPlayerKotlin: MovingPlayerKotlin) : Screen {
         // set the position of the camera in the middle of the screen
         camera.position.set(GameInfo.WIDTH/2f, GameInfo.HEIGHT/2f, 0f)
         camera.update()
+
+        imageBackground = Texture("Game BG.png")
+        turtle = Sprite(Texture("turtle.png"))
+        turtle.setPosition((GameInfo.WIDTH/2).toFloat(), 0f)
     }
 
     override fun render(delta: Float) {
@@ -41,6 +43,7 @@ class MainMenu (private var movingPlayerKotlin: MovingPlayerKotlin) : Screen {
 
         movingPlayerKotlin.getBatch.begin()
         movingPlayerKotlin.getBatch.draw(imageBackground, 0f, 0f, GameInfo.WIDTH.toFloat(), GameInfo.HEIGHT.toFloat())
+        movingPlayerKotlin.getBatch.draw(turtle, turtle.x, turtle.y, 200f, 200f)
         movingPlayerKotlin.getBatch.end()
     }
 
@@ -63,6 +66,7 @@ class MainMenu (private var movingPlayerKotlin: MovingPlayerKotlin) : Screen {
     override fun dispose() {
         movingPlayerKotlin.getBatch.dispose()
         imageBackground.dispose()
+        turtle.texture.dispose()
     }
 
 }
--- core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
+++ core/src/main/kotlin/com/susanafigueroa/MovingPlayerKotlin.kt
@@ -1,13 +1,7 @@
 package com.susanafigueroa
 
 import com.badlogic.gdx.Game
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.utils.ScreenUtils
-import com.badlogic.gdx.utils.viewport.StretchViewport
-import helpers.GameInfo
 import scenes.MainMenu
 
 class MovingPlayerKotlin : Game() {
@@ -25,33 +19,4 @@ class MovingPlayerKotlin : Game() {
     override fun render() {
         super.render()
     }
-
-    /*
-    private lateinit var turtle: Sprite
-
-
-
-
-    override fun create() {
-
-
-
-
-        turtle = Sprite(Texture("turtle.png"))
-
-        turtle.setPosition((GameInfo.WIDTH/2).toFloat(), 0f)
-    }
-
-    override fun render() {
-batch.draw(turtle, turtle.x, turtle.y, 200f, 200f)
-    }
-
-    override fun resize(width: Int, height: Int) {
-
-    }
-
-    override fun dispose() {
-
-        turtle.getTexture().dispose()
-    }*/
 }
