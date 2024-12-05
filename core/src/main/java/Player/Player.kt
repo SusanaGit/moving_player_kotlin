b2908d3f11bf7aa2b386233cf2c934afeaf616fb
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-05 18:31:16 +0100

    add Box2D dependency to core module for physics simulation

--- core/src/main/java/Player/Player.kt
+++ core/src/main/java/Player/Player.kt
@@ -2,12 +2,15 @@ package Player
 
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.physics.box2d.World
 
-class Player(
-    name: String,
-    x: Float,
-    y: Float
+class Player( name: String, x: Float, y: Float
 ) : Sprite(Texture(name)) {
+
+    private lateinit var world: World
+    private lateinit var body: Body
+
     init {
         setSize(200f, 200f)
         setPosition(x - width / 2, y - height / 2)
